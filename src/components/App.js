74d66a4e279a4af9e60a770f43639773e2b1c495
ldelbel <lucdelbel@gmail.com>
2021-01-27 18:11:49 -0300

    Add App, Button, ButtonPanel, and Display components

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import '../App.css';
+import Display from './Display';
+import ButtonPanel from './ButtonPanel';
+
+const App = () => (
+  <div className="App">
+    <Display />
+    <ButtonPanel />
+  </div>
+);
+
+export default App;
